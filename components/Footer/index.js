ee96c56560bb4ab4474d367008285737c421082d
hesham <heshamelmasry1992@gmail.com>
2018-09-07 16:24:55 +0200

    footer

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -1,9 +1,25 @@
+import React from 'react'
+// import Link from 'next/link'
 import styled from 'styled-components'
 
-const Footer = styled.footer`
+const Wrapper = styled.nav`
   padding: 15px;
+  border-bottom: 1px solid #ddd;
+  display: flex;
   background: #000000;
-  color: white
+
+  a {
+    padding: 0 15px;
+    color: #FFF;
+  }
+  .footer {
+  }
 `
 
-export default Footer
+const Nav = () => (
+  <Wrapper>
+    <div className='footer' />
+  </Wrapper>
+)
+
+export default Nav
--- layouts/Main.js
+++ layouts/Main.js
@@ -1,23 +1,26 @@
+import React from 'react'
 import Head from 'next/head'
 import Wrapper from './Wrapper'
 import Nav from 'components/Nav'
 import Footer from 'components/Footer'
 
-export default ({ children, title = 'HomeFoodie - Matches cravers with creators.' }) => (
-  <Wrapper>
-    <Head>
-      <title>{ title }</title>
-    </Head>
-    <header>
-      <Nav />
-    </header>
+export default ({children, title = 'HomeFoodie - Matches cravers with creators.'}) => {
+  return (
+    <div>
+      <Wrapper>
+        <Head>
+          <title>{title}</title>
+        </Head>
+        <header>
+          <Nav />
+        </header>
 
-    <main>
-      { children }
-    </main>
+        <main>
+          {children}
+        </main>
 
-    <Footer>
-      Footer
-    </Footer>
-  </Wrapper>
-)
+        <Footer />
+      </Wrapper>
+    </div>
+  )
+}
